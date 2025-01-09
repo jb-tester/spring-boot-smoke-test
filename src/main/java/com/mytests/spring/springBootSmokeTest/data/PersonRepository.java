19ce0cee3c6c5d14f23d28a433ca05d9d52b9646
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-01-09 15:57:09 +0100

    sort

--- src/main/java/com/mytests/spring/springBootSmokeTest/data/PersonRepository.java
+++ src/main/java/com/mytests/spring/springBootSmokeTest/data/PersonRepository.java
@@ -1,5 +1,6 @@
 package com.mytests.spring.springBootSmokeTest.data;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
